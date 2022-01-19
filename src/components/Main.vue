a573fecf3f256218062b5970e16c12a705a6580c
Angeloamenta <angeloamenta7@gmail.com>
2022-01-19 11:51:01 +0100

    creazione array-stampa titoli

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Header</div>
+  <header>
+      <ul>
+          <li v-for="(film, index) in arrayFilm"
+          :key="index">
+          {{film.title}}
+          </li>
+      </ul>
+  </header>
 </template>
 
 <script>
@@ -8,15 +15,16 @@ export default {
 name: "Main",
 data() {
     return {
-    nameFilm: "Ritorno"
+    nameFilm: "Ritorno",
+    arrayFilm: [],
     }
 },
 mounted() {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=8698b0b53840a7e8db92ce738b54cc39&language=en-US&query=${this.nameFilm}&page=1&include_adult=false`)
     .then((result) => {
         console.log(result.data);
-        
-
+        this.arrayFilm = result.data.results,
+        console.log("array", this.arrayFilm);
     })
     .catch((error) => {
         console.log(error);
